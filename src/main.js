1d3f5b684e252ab8ec0221a6dacaf9c7265e100c
yjm <1519484516@qq.com>
2019-01-16 17:42:31 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import {Button} from 'mint-ui'
+// 引入mint-ui css
+import 'mint-ui/lib/style.css'
 
 Vue.config.productionTip = false
-
+// 注册全局组件标签
+Vue.component(Button.name, Button) // <mt-button>
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
